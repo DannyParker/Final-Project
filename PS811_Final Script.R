df7a9dd1b7be67da7778349073e489df65e51767
DannyParker <60512960+DannyParker@users.noreply.github.com>
2020-04-17 09:44:03 -0400

    Add files via upload

---
+++ PS811_Final Script.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("/Users/Keisha/Desktop/PS811_Final")
+
+library("here")
+library("magrittr")
+library("tidyverse")
+
+#Later, when my data is ready, I will add my data to this file
+#To find instructions to downloading my data, go to:
+#script 02_data-manipulation.R and start reading that
+#script at line 25
+
